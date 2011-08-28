571b673e2dfcce8452dc75908e7981cc7f87f43c
Yuki Atoh <yuki.atoh@gmail.com>
2011-08-28 00:00:00 +0000

    version 0.2-1

--- R/viopoints.R
+++ R/viopoints.R
@@ -30,48 +30,65 @@ viopoints.formula <- function(formula, data=NULL, ..., subset, na.action=NULL)
   }
 }
 
-viopoints.default <- function(x, ..., groups, at, horizontal=FALSE,
-  jitter=0.2, density.par=list(na.rm=TRUE), 
+viopoints.default <- function(x, ..., groups, na.group=FALSE,
+  method="violin", side="both", amount=jitter, jitter=0.2, offset=1, 
+  density.par=list(na.rm=TRUE), horizontal=!vertical, vertical=TRUE, at,
   points=TRUE, pch=par("pch"), cex=par("cex"), col="red", bg="pink",
   lines=FALSE, line.lty=par("lty"), line.lwd=0.5, line.col="lightgray", 
-  add=FALSE, axis.par, group.names, main="", xlab="", ylab="", 
-  xlim, ylim, log="") 
+  add=FALSE, axes=TRUE, frame.plot=axes, axis.par, group.names, 
+  main="", sub="", xlab, ylab, dlab="", glab="", xlim, ylim, log="") 
 {
+  method <- pmatch(method, c("overplot", "stack", "jitter", "violin"))[1L]
+  if (is.na(method) || method == 0L)
+    stop("invalid plotting method")
+  side <- pmatch(side, c("negative", "both", "positive"), duplicates.ok=TRUE)
+  if (is.na(side) || side == 0L)
+    stop("invalid plotting side")
+
   unbind <- function(x, margin=2) {
     if (is.list(x)) return(lapply(x, unlist))
     if (is.matrix(x)) return(unlist(apply(x, margin, list), recursive=FALSE))
     if (is.vector(x)) return(list(x))
   }
   rebind <- function(L) unlist(lapply(L, unbind), recursive=FALSE)
+  seqs <- function(x, side) {
+    l <- length(x)
+    if (side == 1L) return(0.5 - seq_len(l))
+    if (side == 2L) return(seq_len(l) - (l + 1) / 2) 
+    if (side == 3L) return(seq_len(l) - 0.5)
+  }
 
   x <- rebind(list(x,...))
   n <- length(x)
   if (missing(groups)) {
     m <- 1L
+    mn <- n
     reps <- function(x) rep(x, length.out=n)
   } else {
     if (!is.factor(groups)) {
       if (is.list(groups))
         groups <- unlist(groups)
+      groups <- factor(groups, exclude=NULL)
     }
-    groups <- factor(groups, exclude=NULL)
     f <- as.integer(groups)
-    m <- max(f)
-    if (is.na(levels(groups)[m]))
+    m <- nlevels(groups)
+    if (is.na(levels(groups)[m]) && !na.group) 
       m <- m - 1
+    mn <- m * n
     reps <- function(x) {
       if (length(x) <= m) 
         rep(rep(x, length.out=m), each=n) 
       else
-        rep(x, length.out=(m * n))
+        rep(x, length.out=mn)
     }
   }
   if (points)
     pp <- lapply(list(pch=pch, cex=cex, col=col, bg=bg), reps)
   if (lines) 
     lp <- lapply(list(lty=line.lty, lwd=line.lwd, col=line.col), reps)
-  at <- if (missing(at)) seq_len(m * n) else rep(at, length.out=(m * n))
-  
+  side <- rep(side, length.out=mn)
+  at <- if (missing(at)) seq_len(mn) else rep(at, length.out=mn)
+
   if (!add) {
     uat <- unique(at)
     if (missing(group.names)) {
@@ -81,29 +98,34 @@ viopoints.default <- function(x, ..., groups, at, horizontal=FALSE,
         group.names <- if (is.null(names(x))) uat else names(x)
     }
     group.names <- rep(group.names, length=length(uat))
+    group.names[is.na(group.names)] <- "<NA>"
     plot.new()
     if (horizontal) {
       if (missing(xlim)) 
-        xlim <- range(unlist(x), finite=TRUE)
+        xlim <- range(unlist(x, use.names=FALSE), finite=TRUE)
       if (missing(ylim)) 
         ylim <- range(uat) + 
           ifelse(length(uat) < 2L, 1, min(diff(sort(uat)))) * c(-0.5,0.5)
       plot.window(xlim=xlim, ylim=ylim, log=log)
-      axis(1)
-      if (missing(axis.par))
-        axis.par <- list(at=uat, labels=group.names)
-      do.call("axis", c(list(2), axis.par))
+      if (axes) {
+        axis(1)
+        if (missing(axis.par))
+          axis.par <- list(at=uat, labels=group.names)
+        do.call("axis", c(list(2), axis.par))
+      }
     } else {
       if (missing(xlim))    
         xlim <- range(uat) + 
           ifelse(length(uat) < 2L, 1, min(diff(sort(uat)))) * c(-0.5,0.5)
       if (missing(ylim))
-        ylim <- range(unlist(x), finite=TRUE)
+        ylim <- range(unlist(x, use.names=FALSE), finite=TRUE)
       plot.window(xlim=xlim, ylim=ylim, log=log)
-      if (missing(axis.par))
-        axis.par <- list(at=uat, labels=group.names)
-      do.call("axis", c(list(1), axis.par))
-      axis(2)
+      if (axes) {
+        if (missing(axis.par))
+          axis.par <- list(at=uat, labels=group.names)
+        do.call("axis", c(list(1), axis.par))
+        axis(2)
+      }
     }
   }
 
@@ -134,6 +156,9 @@ viopoints.default <- function(x, ..., groups, at, horizontal=FALSE,
   }
 
   if (points) {
+    cxy <- par("cxy")
+    offset <- offset * cex * if (horizontal) cxy[2L]/3 else cxy[1L]/2
+
     for (i in seq_len(m)) {
       k <- (i - 1) * n
       for (j in seq_len(n)) {
@@ -143,14 +168,30 @@ viopoints.default <- function(x, ..., groups, at, horizontal=FALSE,
           f1 <- rep(f, length=length(z1))
           z1 <- z1[f1==i]
         }
-        l <- length(z1)
+        l <- sum(!is.na(z1))
         if (l <= 0L) next
-        if (1L < l & 0 < jitter) {
-          d <- do.call("density", c(list(z1), density.par))
-          d <- approx(d$x, d$y, z1)$y
-          d <- jitter * d / max(d, na.rm=TRUE) * runif(l, -1, 1)
+        if (l <= 1L || method == 1L) { # overplot
+          d <- rep(0, length(z1))
+        } else if (method == 2L) { # stack
+          z2 <- split(z1, factor(z1))
+          z1 <- unlist(z2, use.names = FALSE)
+          d <- unlist(lapply(z2, seqs, side[k]), use.names = FALSE) * offset
+          if (!is.na(amount)) {
+            r <- max(abs(d), na.rm=TRUE)
+            if (0 < r)
+              d <- d / r * amount
+          }
         } else {
-          d <- 0
+          if (method == 3L) { # jitter
+            d <- amount      
+          } else { # violin
+            d <- do.call(getFromNamespace("density", "stats"), 
+              c(list(z1), density.par))
+            d <- stats::approx(d$x, d$y, z1)$y
+            d <- amount * d / max(d, na.rm=TRUE)
+          }
+          r <- if (side[k]==1L) c(-1,0) else if (side[k]==2L) c(-1,1) else c(0,1)
+          d <- d * stats::runif(length(z1), r[1], r[2])
         }
         if (horizontal) {
           points(z1, at[k] + d,
@@ -164,8 +205,10 @@ viopoints.default <- function(x, ..., groups, at, horizontal=FALSE,
   }
 
   if (!add) {
-    title(main=main, xlab=xlab, ylab=ylab)  
-    box()
+    if (missing(xlab)) xlab <- if (horizontal) dlab else glab
+    if (missing(ylab)) ylab <- if (horizontal) glab else dlab
+    title(main=main, sub=sub, xlab=xlab, ylab=ylab)  
+    if (frame.plot) box()
   }
 
   invisible(at)
--- tests/test_viopoints.R
+++ tests/test_viopoints.R
@@ -6,8 +6,10 @@ viopoints(iris[1])
 viopoints(iris[1:4])
 
 # groups
-viopoints(iris[1], groups=iris[5], col=2:4)
-viopoints(iris[1], groups=iris[5], at=1, col=2:4)
+at <- viopoints(iris[1], groups=iris[5], col=2:4)
+at
+at <- viopoints(iris[1], groups=iris[5], at=1, col=2:4)
+at
 
 # data.frame
 viopoints(iris[1:4], groups=iris[5], at=1:4, col=2:4)
@@ -34,9 +36,41 @@ viopoints(as.matrix(iris[,1:4]) ~ 1)
 viopoints(Sepal.Width ~ Species + cut(Sepal.Width, 2), data=iris, col=rainbow(6, v=0.8))
 viopoints(Sepal.Width ~ Species + cut(Sepal.Width, 2), data=iris, at=1:3, col=rainbow(6, v=0.8))
 
-# main, xlab, ylab
-viopoints(Sepal.Length ~ Species, data=iris, col=2:4, main="Title", xlab="Species", ylab="Sepal.Length")
-viopoints(Sepal.Length ~ Species, data=iris, col=2:4, horizontal=TRUE, main="Title", xlab="Sepal.Length", ylab="Species")
+x <- stats::rnorm(100)
+
+# method
+method <- c("overplot", "jitter", "stack", "violin")
+viopoints(x, method="overplot", xlim=c(0,4), at=0.5, glab="method", axis.par=list(at=seq(0.5,3.5), labels=method))
+viopoints(x, method="jitter", at=1.5, add=TRUE)
+viopoints(round(x,1), method="stack", at=2.5, add=TRUE)
+viopoints(x, method="violin", at=3.5, add=TRUE)
+
+viopoints(x, method="overplot", horizontal=TRUE, ylim=c(0,4), at=0.5, glab="method", axis.par=list(at=seq(0.5,3.5), labels=method))
+viopoints(x, method="jitter", horizontal=TRUE, at=1.5, add=TRUE)
+viopoints(round(x,1), method="stack", horizontal=TRUE, at=2.5, add=TRUE)
+viopoints(x, method="violin", horizontal=TRUE, at=3.5, add=TRUE)
+
+# amount, offset
+viopoints(round(x,1), method="stack", side="p", at=0.5, xlim=c(0,4), amount=0.2, glab="amount", main="stack", axis.par=list(at=seq(0.5,3.5), labels=c("0.3","0.5","NA","NA,0.5")))
+viopoints(round(x,1), method="stack", side="p", at=1.5, amount=0.3, add=TRUE) 
+viopoints(round(x,1), method="stack", side="p", at=2.5, col=3, amount=NA, add=TRUE) 
+viopoints(round(x,1), method="stack", side="p", at=3.5, col=3, amount=NA, offset=0.5, add=TRUE) 
+
+viopoints(round(x,1), horizontal=TRUE, method="stack", side="p", at=0.5, ylim=c(0,4), amount=0.2, glab="amount", main="stack", axis.par=list(at=seq(0.5,3.5), labels=c("0.3","0.5","NA","NA,0.5")))
+viopoints(round(x,1), horizontal=TRUE, method="stack", side="p", at=1.5, amount=0.3, add=TRUE) 
+viopoints(round(x,1), horizontal=TRUE, method="stack", side="p", at=2.5, col=3, amount=NA, add=TRUE) 
+viopoints(round(x,1), horizontal=TRUE, method="stack", side="p", at=3.5, col=3, amount=NA, offset=0.5, add=TRUE) 
+
+# side
+side <- c("negative", "both", "positive")
+viopoints(x, x, x, side=side, main="rnorm(100)", glab="side", group.names=side)
+viopoints(x, x, x, horizontal=TRUE, side=side, main="rnorm(100)", glab="side", group.names=side)
+
+# main, sub, xlab, ylab, dlab, glab
+viopoints(iris[1:4], main="main", sub="sub", xlab="xlab", ylab="ylab")
+viopoints(iris[1:4], horizontal=TRUE, main="main", sub="sub", xlab="xlab", ylab="ylab")
+viopoints(iris[1:4], main="main", sub="sub", dlab="dlab", glab="glab")
+viopoints(iris[1:4], horizontal=TRUE, main="main", sub="sub", dlab="dlab", glab="glab")
 
 # lines, line.lty, line.lwd, line.col 
 viopoints(iris[,1:4], groups=iris[,5], col=2:4, lines=TRUE)
@@ -71,6 +105,11 @@ viopoints(as.matrix(iris[1:4]) ~ Species, data=iris, jitter=0.3, col=2:4, axis.p
 viopoints(iris[3], density.par=list(na.rm=TRUE, bw="nrd0"), main="nrd0")
 viopoints(iris[3], density.par=list(na.rm=TRUE, bw="SJ-ste"), main="SJ-ste")
 
+# frame.plot, axes
+viopoints(iris[1:4], axes=FALSE)
+viopoints(iris[1:4], frame.plot=FALSE, axes=TRUE)
+viopoints(iris[1:4], frame.plot=TRUE, axes=FALSE)
+
 # NA
 na_iris <- iris
 na_iris[1,1] <- NA
@@ -81,3 +120,15 @@ viopoints(na_iris[1])
 viopoints(na_iris[1], groups=na_iris[5])
 viopoints(Sepal.Length ~ 1, data=na_iris)
 viopoints(Sepal.Length ~ Species, data=na_iris)
+viopoints(na_iris[1], groups=na_iris[5], na.group=TRUE)
+viopoints(Sepal.Length ~ Species, data=na_iris, na.action=na.pass, na.group=TRUE)
+viopoints(na_iris[2], groups=na_iris[5], na.group=TRUE)
+viopoints(Sepal.Width ~ Species, data=na_iris, na.action=na.pass, na.group=TRUE)
+
+for (method in c("overplot", "jitter", "stack", "violin")) {
+  viopoints(na_iris[1:4], method=method, main=method)
+  viopoints(na_iris[3], groups=na_iris[5], method=method, col=2:4, main=method)
+  viopoints(na_iris[3], groups=na_iris[5], na.group=TRUE, method=method, col=2:4, main=method)
+}
+
+
